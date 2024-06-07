ccba0443f0a319067ab9bb6a06a7445a32ccabec
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2024-06-07 03:52:10 -0400

    intial game board

--- Tic-toc-toe/src/App.jsx
+++ Tic-toc-toe/src/App.jsx
@@ -1,10 +1,21 @@
-
 function App() {
-  
-
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player-name">Player 1</span>
+            <span className="player-symbol">X</span>
+          </li>
+          <li>
+            <span className="player-name">Player 2</span>
+            <span className="player-symbol">O</span>
+          </li>
+        </ol>
+        Game Board
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
