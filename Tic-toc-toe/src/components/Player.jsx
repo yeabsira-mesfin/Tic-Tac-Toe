4029b1088827d7c43fde5a39d4f03815d9ec97b4
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2024-06-07 07:00:49 -0400

    onchange function

--- Tic-toc-toe/src/components/Player.jsx
+++ Tic-toc-toe/src/components/Player.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { useState } from "react";
-const Player = ({name,symbol}) => {
+const Player = ({initialName,symbol}) => {
+    const [playerName,setPlayerName] = useState(initialName)
     const [isEditing,setIsEditing] = useState(false);
     function handlEditClick(){
         setIsEditing((editing)=>!editing)
     }
-   
+    function handleChange(event){
+        // console.log(event)
+        setPlayerName(event.target.value)
+    }
     let buttonCaption = 'Edit';
     if(isEditing){
         buttonCaption = 'Save'
@@ -14,14 +18,14 @@ const Player = ({name,symbol}) => {
     <li>
       <span className="player">
         {isEditing ? (
-            <span><input type="text" required value={name}/></span>
+            <span><input type="text" required value={playerName} onChange={handleChange}/></span>
         ) : (
-       <> <span className="player-name">{name}</span>
+       <> <span className="player-name">{playerName}</span>
       </>
     )}
           <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={handlEditClick}>{buttonCaption}</button>
+      <button onClick={handlEditClick} >{buttonCaption}</button>
     </li>
   );
 };
