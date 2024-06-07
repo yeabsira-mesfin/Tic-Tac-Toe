c2f5e4a37c44afb61fbb76a80b3d0b357a9e517c
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2024-06-07 04:20:46 -0400

    Button functionalities"

--- Tic-toc-toe/src/components/Player.jsx
+++ Tic-toc-toe/src/components/Player.jsx
@@ -1,13 +1,25 @@
 import React from "react";
-
+import { useState } from "react";
 const Player = ({name,symbol}) => {
+    const [isEditing,setIsEditing] = useState(false);
+    function handlEditClick(){
+        setIsEditing(true)
+    }
+    function toFalse(){
+        setIsEditing(false)
+    }
   return (
     <li>
       <span id="player">
-        <span className="player-name">{name}</span>
-        <span className="player-symbol">{symbol}</span>
+        {isEditing ? (
+            <span><input type="text" required/></span>
+        ) : (
+       <> <span className="player-name">{name}</span>
+        <span className="player-symbol">{symbol}</span></>
+    )}
+        
       </span>
-      <button>Edit</button>
+      <button onClick={!isEditing?handlEditClick:toFalse}>Edit</button>
     </li>
   );
 };
