71c15e001846cb1e4614b1aada149d901c04b5cb
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2024-06-10 06:02:10 -0400

    Replaced active player by a function

--- Tic-toc-toe/src/App.jsx
+++ Tic-toc-toe/src/App.jsx
@@ -2,17 +2,22 @@ import Player from "./components/Player";
 import { useState } from "react";
 import GameBoard from "./components/GameBoard";
 import Log from "./components/Log";
+
+function derivedActivePlayer(gameTurns){
+  let currentPlayer = 'X';
+  if(gameTurns.length > 0 && gameTurns[0].player === 'X'){
+    currentPlayer = 'O';
+  }
+  return currentPlayer;
+}
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  const [activePlayer, setAcitvePlayer] = useState("X");
-
+  // const [activePlayer, setAcitvePlayer] = useState("X");
+  const activePlayer = derivedActivePlayer(gameTurns);
   function handleSelectSquare(rowIndex, colIndex) {
-    setAcitvePlayer((curActviePlayer) => (curActviePlayer === "X" ? "O" : "X"));
+    // setAcitvePlayer((curActviePlayer) => (curActviePlayer === "X" ? "O" : "X"));
     setGameTurns((prevTurns) => {
-      let currentPlayer = 'X';
-      if(prevTurns.length>0 && prevTurns[0].player === 'X'){
-        currentPlayer = 'O'
-      }
+      const currentPlayer = derivedActivePlayer(prevTurns);
       const updatedTurns = [
         { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
         ...prevTurns,
@@ -41,7 +46,7 @@ function App() {
           turns ={gameTurns}
         />
       </div>
-      <Log />
+      <Log turns = {gameTurns}/>
     </main>
   );
 }
--- Tic-toc-toe/src/components/Log.jsx
+++ Tic-toc-toe/src/components/Log.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Log = () => {
+const Log = ({turns,}) => {
   return (
     <ol id='log'>
-        
+        {turns.map(turn=> <li key={`${turn.square.row}${turn.square.col}`}>{turn.player} selected {turn.square.row},{turn.square.col}</li>)}
     </ol>
   )
 }
